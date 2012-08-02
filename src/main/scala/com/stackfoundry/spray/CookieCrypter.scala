0af5af20317049d83bb262831875a9b3df44f417
Yann Ramin <atrus@stackworks.net>
2012-08-02 00:14:09 -0700

    Start supporting AES, need to have a configured IV

--- src/main/scala/com/stackfoundry/spray/CookieCrypter.scala
+++ src/main/scala/com/stackfoundry/spray/CookieCrypter.scala
@@ -5,11 +5,21 @@ import javax.crypto.{Cipher, Mac}
 import sun.misc.{BASE64Decoder, BASE64Encoder}
 
 trait CookieRC4 {
-   val cipherAlgo = "RC4"
+	val cipherAlgo = "RC4"
+	val keyType = "RC4"
 }
 
 trait CookieSHA1 {
-   val macAlgo = "HmacSHA1"
+	val macAlgo = "HmacSHA1"
+}
+
+trait CookieMD5 {
+	val macAlgo = "HmacMD5"
+}
+
+trait CookieAES {
+	val cipherAlgo = "AES/CBC/PKCS5Padding"
+	val keyType = "AES"
 }
 
 /**
@@ -20,57 +30,62 @@ trait CookieSHA1 {
 trait CookieCrypter {
 
 
-  val macAlgo : String
-  val cipherAlgo : String
+	val macAlgo : String
+	val cipherAlgo : String
+	val keyType : String
 
-  protected val defaultExpires: Long = 60*60*24*5
-  protected val cipherKeySpec: SecretKeySpec
-  protected val hmacKeySpec: SecretKeySpec
+	protected val defaultExpires: Long = 60*60*24*5
 
-  def verify(inp: String, extra: String = "") : Option[String] = {
-    val decoder = new BASE64Decoder()
-    val now = System.currentTimeMillis() / 1000
+	protected val cipherKey: Array[Byte]
+	protected val hmacKey: Array[Byte]
 
-    val decoded = decoder.decodeBuffer(inp)
-    val cipher = Cipher.getInstance(cipherAlgo)
-    cipher.init(Cipher.DECRYPT_MODE, cipherKeySpec)
-    val clear = cipher.doFinal(decoded)
+	protected lazy val cipherKeySpec = new SecretKeySpec(cipherKey, keyType)
+	protected lazy val hmacKeySpec = new SecretKeySpec(hmacKey, macAlgo)
 
-    val (rest, msg_time_bytes) = clear.splitAt(clear.size - 4)
-    val msg_time = ((msg_time_bytes(0) << 24) & 0xFF000000) | ((msg_time_bytes(1) << 16) & 0xFF0000) | ((msg_time_bytes(2) << 8) & 0xFF00) | (msg_time_bytes(3) & 0xFF)
+	def verify(inp: String, extra: String = "") : Option[String] = {
+		val decoder = new BASE64Decoder()
+		val now = System.currentTimeMillis() / 1000
 
-    val mac = Mac.getInstance(macAlgo)
-    mac.init(hmacKeySpec)
-    val (hmac, message) = rest.splitAt(mac.getMacLength)
-    val computed_hmac = mac.doFinal(message ++ msg_time_bytes ++ extra.getBytes("UTF-8"))
+		val decoded = decoder.decodeBuffer(inp)
+		val cipher = Cipher.getInstance(cipherAlgo)
+		cipher.init(Cipher.DECRYPT_MODE, cipherKeySpec)
+		val clear = cipher.doFinal(decoded)
 
-    if (!computed_hmac.sameElements(hmac))
-      None
-    else if (msg_time < now)
-      None
-    else
-      Some(new String(message, "UTF-8"))
+		val (rest, msg_time_bytes) = clear.splitAt(clear.size - 4)
+		val msg_time = ((msg_time_bytes(0) << 24) & 0xFF000000) | ((msg_time_bytes(1) << 16) & 0xFF0000) | ((msg_time_bytes(2) << 8) & 0xFF00) | (msg_time_bytes(3) & 0xFF)
 
-  }
+		val mac = Mac.getInstance(macAlgo)
+		mac.init(hmacKeySpec)
+		val (hmac, message) = rest.splitAt(mac.getMacLength)
+		val computed_hmac = mac.doFinal(message ++ msg_time_bytes ++ extra.getBytes("UTF-8"))
 
-  def output(inp: String, extra: String = "", expires: Long = defaultExpires) : String = {
+		if (!computed_hmac.sameElements(hmac))
+			None
+		else if (msg_time < now)
+			None
+		else
+			Some(new String(message, "UTF-8"))
 
-    val encoder = new BASE64Encoder()
+	}
 
-    // First append the time to the message
-    val now = expires + (System.currentTimeMillis() / 1000)
-    val now_bytes = List((now >> 24) & 0xFF, (now >> 16) & 0xFF, (now >> 8) & 0xFF, now & 0xFF).map(_.toByte).toArray
-    val in_bytes = inp.getBytes("UTF-8") ++ now_bytes
+	def output(inp: String, extra: String = "", expires: Long = defaultExpires) : String = {
 
-    val mac = Mac.getInstance(macAlgo)
-    mac.init(hmacKeySpec)
-    val hmac = mac.doFinal(in_bytes ++ extra.getBytes("UTF-8"))
+		val encoder = new BASE64Encoder()
 
-    val to_cipher = hmac ++ in_bytes
-    val cipher = Cipher.getInstance(cipherAlgo)
-    cipher.init(Cipher.ENCRYPT_MODE, cipherKeySpec)
-    val ciphered = cipher.doFinal(to_cipher)
+		// First append the time to the message
+		val now = expires + (System.currentTimeMillis() / 1000)
+		val now_bytes = List((now >> 24) & 0xFF, (now >> 16) & 0xFF, (now >> 8) & 0xFF, now & 0xFF).map(_.toByte).toArray
+		val in_bytes = inp.getBytes("UTF-8") ++ now_bytes
 
-    encoder.encode(ciphered)
-  }
+		val mac = Mac.getInstance(macAlgo)
+		mac.init(hmacKeySpec)
+		val hmac = mac.doFinal(in_bytes ++ extra.getBytes("UTF-8"))
+
+		val to_cipher = hmac ++ in_bytes
+		val cipher = Cipher.getInstance(cipherAlgo)
+		cipher.init(Cipher.ENCRYPT_MODE, cipherKeySpec)
+		val ciphered = cipher.doFinal(to_cipher)
+
+		encoder.encode(ciphered)
+	}
 }
--- src/test/scala/com/stackfoundry/spray/CookieCrypter$Test.scala
+++ src/test/scala/com/stackfoundry/spray/CookieCrypter$Test.scala
@@ -4,28 +4,25 @@ import org.specs2.mutable.Specification
 import javax.crypto.spec.SecretKeySpec
 
 object DemoKeys {
-  val hmacKeySpec = new SecretKeySpec(
-    "qnscAdgRlkIhAdPY44oiexBKtQbGY0orf7OV1I50".getBytes(),
-    "HmacSHA1")
-
-  val rc4KeySpec = new SecretKeySpec(
-    "abcdefghimnoalqi".getBytes(), "RC4"
-  )
+  val hmacKeySpec = "qnscAdgRlkIhAdPY44oiexBKtQbGY0orf7OV1I50".getBytes()
+  val rc4KeySpec = "abcdefghimnoalqi".getBytes()
 }
 
 class CookieCrypter$Test extends Specification {
 
   "Test building a cookiecrypter" should {
-    val t = new CookieCrypter with CookieSHA1 with CookieRC4 {
-      protected val hmacKeySpec: SecretKeySpec = DemoKeys.hmacKeySpec
-      protected val cipherKeySpec: SecretKeySpec = DemoKeys.rc4KeySpec
+    "work" in {
+      val t = new CookieCrypter with CookieSHA1 with CookieRC4 {
+        protected val hmacKey = DemoKeys.hmacKeySpec
+        protected val cipherKey = DemoKeys.rc4KeySpec
+      }
+      t must not beNull
     }
-    t must not beNull
   }
   "Simple verification" should {
     val t = new CookieCrypter with CookieSHA1 with CookieRC4 {
-      protected val hmacKeySpec: SecretKeySpec = DemoKeys.hmacKeySpec
-      protected val cipherKeySpec: SecretKeySpec = DemoKeys.rc4KeySpec
+      protected val hmacKey = DemoKeys.hmacKeySpec
+      protected val cipherKey = DemoKeys.rc4KeySpec
     }
     "produce output" in {
       t.output("Hello") must not beNull
@@ -38,6 +35,15 @@ class CookieCrypter$Test extends Specification {
     }
     "expire" in {
       t.verify(t.output("Hello", expires = -1)) === None
+    }
+  }
+  "Other algorithms ->" should {
+    "AES" in {
+      val t = new CookieCrypter with CookieMD5 with CookieAES {
+        protected val hmacKey = DemoKeys.hmacKeySpec
+        protected val cipherKey = DemoKeys.rc4KeySpec
+      }
+      t.verify(t.output("I ate a pony")) === Some("I ate a pony")
     }
   }
 
